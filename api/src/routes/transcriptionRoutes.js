1ab6c7e97969e7ca996e1a5768ab34937cbd827a
ryan-bennett <ryan.bennett.au@gmail.com>
2021-03-26 11:20:19 +1000

    Added identity endpoint

--- api/src/routes/transcriptionRoutes.js
+++ api/src/routes/transcriptionRoutes.js
@@ -1,36 +1,15 @@
 const express = require('express');
 
 const {getIdentityId} = require("../util/requestUtils");
-const {
-    deleteTranscription,
-    putTranscription,
-    getTranscription
-} = require("../service/transcriptionService");
+// const {
+//
+// } = require("../service/transcriptionService");
 const router = express.Router();
 
-
-router.get('/:id', (request, response, next) => {
-    const {id} = request.params;
+router.get('/identity', (request, response, next) => {
     const identityId = getIdentityId(request);
-    return getTranscription(id, identityId)
-        .then(transcription => response.json(transcription))
-        .catch(onerror => next(onerror));
+    response.json({identityId});
 })
 
-
-router.delete('/:id', (request, response, next) => {
-    const {id} = request.params;
-    const identityId = getIdentityId(request);
-    return deleteTranscription(id, identityId)
-        .then(transcription => response.json(transcription))
-        .catch(onerror => next(onerror));
-})
-
-router.put('/', (request, response, next) => {
-    const identityId = getIdentityId(request);
-    return putTranscription(request.body, identityId)
-        .then(transcription => response.json(transcription))
-        .catch(onerror => next(onerror));
-})
 
 module.exports = router;
