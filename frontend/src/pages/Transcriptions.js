b5cfc32e609bf9d9df14b36551b6f14090c641fd
ScriptSmith <ScriptSmith@users.noreply.github.com>
2021-03-26 16:09:48 +1000

    Add transcription table with dummy data

--- frontend/src/pages/Transcriptions.js
+++ frontend/src/pages/Transcriptions.js
@@ -1,6 +1,46 @@
 import React, {useState, useEffect, useRef} from 'react';
 import {Storage} from 'aws-amplify';
 import {TranscriptionService} from '../service/TranscriptionService';
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
+import {FileUpload} from 'primereact/fileupload';
+
+const TranscriptionList = () => {
+    const mockData = [
+        {"filename": "Top Gun 2 - Top Gunnier.mp4", "status": "started", "link": "https://www.example.com"},
+        {"filename": "Star Trek VII - The Search for eResearch.mp4", "status": "running", "link": "https://www.example.com"},
+        {"filename": "TRRiffic.mp4", "status": "finished", "link": "https://www.example.com"},
+        {"filename": "Nightmare on Helm street.mp4", "status": "error", "link": "https://www.example.com"},
+    ]
+    const statusIcons = {
+        "started": <span><i className="pi pi-cloud-upload"></i> Started</span>,
+        "running": <span><i className="pi pi-spinner"></i> Running</span>,
+        "finished": <span><i className="pi pi-check"></i> Finished</span>,
+        "error": <span><i className="pi pi-exclamation-triangle"></i> Error</span>,
+    }
+
+
+    const tableData = mockData.map((d) => {
+        let link = <i className="pi pi-download"></i>;
+        if (d['status'] === "finished") {
+            link = <a href={d['link']}>{link}</a>;
+        }
+
+        return {
+            "filename": d['filename'],
+            "status": statusIcons[d['status']],
+            "link": link
+        }
+    })
+
+    return (
+        <DataTable value={tableData}>
+            <Column field="filename" header="File name"></Column>
+            <Column field="status" header="Status"></Column>
+            <Column field="link" header="Download link"></Column>
+        </DataTable>
+    )
+}
 
 export const Transcriptions = () => {
 
@@ -26,8 +66,11 @@ export const Transcriptions = () => {
         }
     }
 
-    return (<input
-        type="file"
-        onChange={onChange}
-    />);
+    return (
+    <React.Fragment>
+        <TranscriptionList/>
+        <br></br>
+        <FileUpload mode="basic" name="demo" url="./upload" chooseLabel="Upload file"></FileUpload>
+    </React.Fragment>
+    );
 }
