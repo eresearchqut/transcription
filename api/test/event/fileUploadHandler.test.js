91f8d006809c4417f8dd69be1e484bbfae942b18
ryan-bennett <ryan.bennett.au@gmail.com>
2021-04-01 14:21:42 +1000

    Added unit test

---
+++ api/test/event/fileUploadHandler.test.js
@@ -0,0 +1,22 @@
+const {handler} = require("../../src/event/fileUploadHandler");
+const {mockClient} = require('aws-sdk-client-mock');
+const {TranscribeClient, StartTranscriptionJobCommand} = require("@aws-sdk/client-transcribe");
+
+describe("config", () => {
+
+    it("start job after file upload", async () => {
+        const transcribeMock = mockClient(TranscribeClient);
+        const fileUploadEvent = require("./fileUplopadEvent.json");
+        transcribeMock
+            .on(StartTranscriptionJobCommand)
+            .resolves({
+                "TranscriptionJob": {
+                    "TranscriptionJobName": "A-Job"
+                }
+            });
+        expect(await handler(fileUploadEvent))
+            .toEqual("Processed 1 uploads");
+    });
+
+
+});
