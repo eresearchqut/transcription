cba25c22d7dfc0bbcac06d538bd4e39650e043ec
ScriptSmith <ScriptSmith@users.noreply.github.com>
2021-04-12 14:36:10 +1000

    Complete download options

--- frontend/src/components/TranscriptionDialog.jsx
+++ frontend/src/components/TranscriptionDialog.jsx
@@ -1,35 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 import { ClipboardButton } from './ClipboardButton';
 
 
 export const TranscriptionDialog = (props) => {
-    const [displayDialog, setDialog] = useState(false);
+    const [dialogDisplayed, setDialogDisplayed] = useState(false);
     const [rawTranscriptionText, setRawTranscriptionText] = useState("");
     const [transcriptionText, setTranscriptionText] = useState(null);
+    const [downloadUrl, setDownloadUrl] = useState("")
+    const [dataDownloadUrl, setDataDownloadUrl] = useState("")
 
-    const displayFullText = async () => {
-        const response = await props.getData();
-        const data = JSON.parse(await response.Body.text());
+    const displayDialog = async () => {
+        const fileUrl = await props.getUrl();
+        setDataDownloadUrl(fileUrl)
 
-        setRawTranscriptionText(data.results.transcripts.map((t) => t.transcript).join(""));
+        const fileContent = await props.getData();
+        const data = JSON.parse(await fileContent.Body.text());
+        const rawText = data.results.transcripts.map((t) => t.transcript).join("");
+
+        setRawTranscriptionText(rawText);
         setTranscriptionText(data.results.transcripts.map((t, i) => <p key={i}>{t.transcript}</p>));
-        setDialog(true);
+
+        const rawUrl = window.URL.createObjectURL(new Blob([rawText]))
+        setDownloadUrl(rawUrl)
+
+        setDialogDisplayed(true);
     }
 
 
     const footer = (
         <div>
             <ClipboardButton text={rawTranscriptionText} />
+            <a href={downloadUrl} download="transcription.txt"><Button label="Download transcription " /></a>
+            <a href={dataDownloadUrl} download="transcription.json"><Button label="Download transcription data" /></a>
         </div>
     );
 
     return (
         <React.Fragment>
-            <Button label="Download" icon="pi pi-external-link" onClick={displayFullText} disabled={props.disabled} />
+            <Button label="Download" icon="pi pi-external-link" onClick={displayDialog} disabled={props.disabled} />
 
-            <Dialog header={props.filename} visible={displayDialog} style={{ width: '50vw' }} onHide={() => setDialog(false)} footer={footer}>
+            <Dialog header={props.filename} visible={dialogDisplayed} style={{ width: '50vw' }} onHide={() => setDialogDisplayed(false)} footer={footer}>
                 {transcriptionText}
             </Dialog>
         </React.Fragment>
