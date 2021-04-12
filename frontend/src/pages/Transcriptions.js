75c32ceca3ec1c52a815924b0ae3039679457748
ScriptSmith <ScriptSmith@users.noreply.github.com>
2021-04-12 13:12:54 +1000

    Poll every 10 seconds

--- frontend/src/pages/Transcriptions.js
+++ frontend/src/pages/Transcriptions.js
@@ -1,12 +1,31 @@
-import React, {useState, useEffect, useRef} from 'react';
-import {TranscriptionService} from '../service/TranscriptionService';
-import {UserService} from '../service/UserService';
+import React, { useState, useEffect, useRef } from 'react';
+import { TranscriptionService } from '../service/TranscriptionService';
+import { UserService } from '../service/UserService';
 
-import {S3FileUpload} from '../components/S3FileUpload';
-import {TranscriptionList} from '../components/TranscriptionList';
-import {Toast} from 'primereact/toast';
+import { S3FileUpload } from '../components/S3FileUpload';
+import { TranscriptionList } from '../components/TranscriptionList';
+import { Toast } from 'primereact/toast';
 
+function useInterval(callback, delay) {
+    const savedCallback = useRef();
 
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        tick()
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+}
 
 
 export const Transcriptions = () => {
@@ -19,25 +38,26 @@ export const Transcriptions = () => {
     const toast = useRef(null);
 
     useEffect(() => {
-
         userService.getUser().then(result => setUser(result));
-        transcriptionService.getTranscriptions().then(result => setTranascriptions(result));
-
     }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    const onUpload = ({file}) => {
-        toast.current.show({severity: "info", summary: "Success", detail: `${file.name} uploaded`})
+    useInterval(() => {
+        transcriptionService.getTranscriptions().then(result => setTranascriptions(result));
+    }, 10000)
+
+    const onUpload = ({ file }) => {
+        toast.current.show({ severity: "info", summary: "Success", detail: `${file.name} uploaded` })
     }
 
-    const onError = ({file, error}) => {
-        toast.current.show({severity: "error", summary: "Failure", detail: `${file.name} failed to upload`})
+    const onError = ({ file, error }) => {
+        toast.current.show({ severity: "error", summary: "Failure", detail: `${file.name} failed to upload` })
     }
 
     return (
         <React.Fragment>
-            <Toast ref={toast}/>
+            <Toast ref={toast} />
 
-            <TranscriptionList transcriptions={transcriptions}/>
+            <TranscriptionList transcriptions={transcriptions} />
             <br></br>
             <S3FileUpload progress={50} mode="advanced" uploadDir={uploadDir} customUpload onUpload={onUpload} onError={onError} multiple chooseLabel="Add files" />
         </React.Fragment>
