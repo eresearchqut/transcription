c776173fe6c734f04f6e2f04bbc3e26626bbe1c7
ScriptSmith <ScriptSmith@users.noreply.github.com>
2021-08-16 16:21:39 +1000

    Nicer segment timestamp formatting

--- frontend/src/components/script/TranscriptionScript.jsx
+++ frontend/src/components/script/TranscriptionScript.jsx
@@ -2,6 +2,18 @@ import { React, useEffect } from "react";
 
 import { DialogueSegment } from "./ScriptSegment";
 
+const formatTime = (totalSeconds) => {
+  const hours = Math.floor(totalSeconds / 3600)
+    .toString()
+    .padStart(2, "0");
+  const remainingSeconds = totalSeconds % 3600;
+  const minutes = ("00" + Math.floor(remainingSeconds / 60)).slice(-2);
+  const seconds = ("00" + Math.floor(remainingSeconds % 60)).slice(-2);
+  const milliseconds = (remainingSeconds % 1).toFixed(3).toString().slice(1, 5);
+
+  return `${hours}:${minutes}:${seconds}${milliseconds}`;
+};
+
 export const TranscriptionScript = (props) => {
   const speakerSegments = props.speakerLabels.segments.map((segment, index) => [
     segment,
@@ -14,8 +26,8 @@ export const TranscriptionScript = (props) => {
     const [speakerSegment, dialogueSegment] = segment;
     const [mainSpeaker, startTime, endTime] = [
       speakerSegment.speaker_label,
-      speakerSegment.start_time,
-      speakerSegment.end_time,
+      formatTime(speakerSegment.start_time),
+      formatTime(speakerSegment.end_time),
     ];
     const transcript = dialogueSegment.alternatives[0].transcript;
 
