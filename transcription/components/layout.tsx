9c34a0c4f1820d6783cabd6dd4520bc353e8be94
ryan-bennett <ryan.bennett.au@gmail.com>
2022-08-30 13:02:51 +1000

    Fixed light and dark mode sections

--- transcription/components/layout.tsx
+++ transcription/components/layout.tsx
@@ -1,6 +1,17 @@
 import {Box, Stack, Text} from "@chakra-ui/layout";
 import React from "react";
-import {Button, Divider, Heading, Image, Link, SimpleGrid, Spacer, useColorMode} from "@chakra-ui/react";
+import {
+    Button,
+    DarkMode,
+    Divider,
+    Heading,
+    Image,
+    LightMode,
+    Link,
+    SimpleGrid,
+    Spacer,
+    useColorMode
+} from "@chakra-ui/react";
 
 
 import {withAnonymous, withAuthentication} from "../context/with-auth";
@@ -27,31 +38,33 @@ export const Layout = ({children}: any) => {
             gridTemplateRows={'60px 1fr 30px 30px'}
         >
             <Box bg={'blue.900'} color={'white'} width={'100%'}>
-                <Stack direction='row' p={2} alignItems={'center'} maxWidth={'1576px'} m={'auto'}>
-                    <Image
-                        alt="QUT logo"
-                        src="logo.png"
-                        width={"40px"}
-                        height={"40px"}
-                    />
-                    <Divider orientation='vertical'/>
-                    <Heading>Transcribe</Heading>
-                    <Spacer/>
-                    <Button onClick={toggleColorMode}>
-                        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-                    </Button>
+                <DarkMode>
+                    <Stack direction='row' p={2} alignItems={'center'} maxWidth={'1576px'} m={'auto'}>
+                        <Image
+                            alt="QUT logo"
+                            src="logo.png"
+                            width={"40px"}
+                            height={"40px"}
+                        />
+                        <Divider orientation='vertical'/>
+                        <Heading>Transcribe</Heading>
+                        <Spacer/>
+                        <Button onClick={toggleColorMode}>
+                            Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+                        </Button>
 
-                    {!isAuthenticated &&
-                        <Button onClick={handleLogin}>
-                            Login
-                        </Button>
-                    }
-                    {isAuthenticated &&
-                        <Button onClick={handleLogout}>
-                            Logout
-                        </Button>
-                    }
-                </Stack>
+                        {!isAuthenticated &&
+                            <Button onClick={handleLogin}>
+                                Login
+                            </Button>
+                        }
+                        {isAuthenticated &&
+                            <Button onClick={handleLogout}>
+                                Logout
+                            </Button>
+                        }
+                    </Stack>
+                </DarkMode>
             </Box>
 
             <Box width={'100%'} p={2}>
@@ -61,19 +74,23 @@ export const Layout = ({children}: any) => {
             </Box>
 
             <Box bg={'blue.900'} color={'white'} width={'100%'}>
-                <Stack direction='row' p={1} alignItems={'center'} maxWidth={'1576px'} m={'auto'}>
-                    <Text color={'white'} as={'b'} fontSize={"sm"} noOfLines={1}>Developed by the Office of
-                        eResearch
-                        QUT</Text>
-                </Stack>
+                <DarkMode>
+                    <Stack direction='row' p={1} alignItems={'center'} maxWidth={'1576px'} m={'auto'}>
+                        <Text color={'white'} as={'b'} fontSize={"sm"} noOfLines={1}>Developed by the Office of
+                            eResearch
+                            QUT</Text>
+                    </Stack>
+                </DarkMode>
             </Box>
 
             <Box bg={'white'} color={'blue.900'} width={'100%'}>
-                <Stack direction='row' p={1} alignItems={'center'} maxWidth={'1576px'} m={'auto'}>
-                    <Link href={"https://www.qut.edu.au/about/indigenous"} isExternal fontSize={"sm"}>
-                        QUT acknowledges the Traditional Owners of the lands where QUT now stands.
-                    </Link>
-                </Stack>
+                <LightMode>
+                    <Stack direction='row' p={1} alignItems={'center'} maxWidth={'1576px'} m={'auto'}>
+                        <Link href={"https://www.qut.edu.au/about/indigenous"} isExternal fontSize={"sm"}>
+                            QUT acknowledges the Traditional Owners of the lands where QUT now stands.
+                        </Link>
+                    </Stack>
+                </LightMode>
             </Box>
         </SimpleGrid>
 
