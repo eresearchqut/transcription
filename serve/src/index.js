7a2a24ed3e568795a6b6f88e949a090d86180c82
ScriptSmith <ScriptSmith@users.noreply.github.com>
2023-10-04 12:20:15 +1000

    Remove logging

--- serve/src/index.js
+++ serve/src/index.js
@@ -3,13 +3,11 @@ const hasExtension = /^\/.*\..+$/;
 /** @type {import ("aws-lambda").CloudFrontRequestHandler} */
 const handler = async (event, context, callback) => {
   const { request } = event.Records[0].cf;
-  console.log(`Request uri: ${request.uri}`);
   if (request.uri === '/') {
     request.uri = '/index.html';
   } else if (!request.uri.match(hasExtension)) {
     request.uri = `${request.uri}.html`;
   }
-  console.log(`Final uri: ${request.uri}`);
   callback(null, request);
 };
 
