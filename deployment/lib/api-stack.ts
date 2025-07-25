be68095229dd25514cd548cb9c4ddd62b73ef36b
ScriptSmith <ScriptSmith@users.noreply.github.com>
2025-07-25 15:44:59 +1000

    Add Umami analytics script

--- deployment/lib/api-stack.ts
+++ deployment/lib/api-stack.ts
@@ -42,6 +42,7 @@ export interface ApiStackProps extends cdk.StackProps {
     SplunkRumAccessToken: string;
     SubnetIds: string[];
     SupportedIdentityProviders: string[];
+    UmamiWebsiteId: string;
     UserPoolStackName: string;
     VpcId: string;
   };
@@ -457,6 +458,7 @@ export class ApiStack extends cdk.Stack {
         NEXT_PUBLIC_AUTH_DOMAIN: cdk.Fn.importValue(`${props.parameters.UserPoolStackName}-DomainName`),
         NEXT_PUBLIC_TRANSCRIPTION_BUCKET: dataBucket.bucketName,
         NEXT_PUBLIC_SPLUNK_RUM_ACCESS_TOKEN: props.parameters.SplunkRumAccessToken,
+        NEXT_PUBLIC_UMAMI_WEBSITE_ID: props.parameters.UmamiWebsiteId,
       })
     });
   }
