4f26820065507e0957ed086fb4de4296d750bf6e
ppettitau <paola.pettit@qut.edu.au>
2025-10-20 10:40:00 +1000

    Revert "Add Umami analytics script"
    This reverts commit be68095229dd25514cd548cb9c4ddd62b73ef36b.

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,13 +4,7 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <script
-            defer
-            src="https://umami.dev.eres.qut.edu.au/script.js"
-            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
-          ></script>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
