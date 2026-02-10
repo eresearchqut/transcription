253d33b3d35e0a2a1372d63348a47607c4dda590
ryan-bennett <ryan.bennett.au@gmail.com>
2026-02-10 17:01:59 +1000

    ERP-3408 - Umami Integration
    Debugging auth in dev

--- serve/src/index.js
+++ serve/src/index.js
@@ -3,7 +3,7 @@ const hasExtension = /^\/.*\..+$/;
 /** @type {import ("aws-lambda").CloudFrontRequestHandler} */
 const handler = async (event) => {
   const { request } = event.Records[0].cf;
-  console.log(request.uri);
+  console.log(JSON.stringify(request));
   if (request.uri === '/') {
     request.uri = '/index.html';
   } else if (!request.uri.match(hasExtension)) {
