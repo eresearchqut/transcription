84bda5ed98aa6f41f010d1c481d996e8c0bf6610
ryan-bennett <ryan.bennett.au@gmail.com>
2026-02-11 10:54:04 +1000

    ERP-3408 - Umami Integration
    Adding deployment env config for UmamiWebsiteId and UmamiUrl

--- deployment/bin/deployment.ts
+++ deployment/bin/deployment.ts
@@ -30,6 +30,8 @@ interface EnvironmentConfig {
     SubnetIds: string[];
     SupportedIdentityProviders: string[];
     UserPoolStackName: string;
+    UmamiWebsiteId: string;
+    UmamiUrl: string;
     VpcId: string;
   };
 }
