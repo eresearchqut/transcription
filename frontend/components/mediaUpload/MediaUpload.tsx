8423424c609ac953a5f628dfe0bf5ebb67621a8f
ScriptSmith <ScriptSmith@users.noreply.github.com>
2026-06-22 14:14:20 +1000

    Update transcriptions button label

--- frontend/components/mediaUpload/MediaUpload.tsx
+++ frontend/components/mediaUpload/MediaUpload.tsx
@@ -241,9 +241,9 @@ export const MediaUpload: FunctionComponent<MediaUploadProps> = ({
             </Group>
             <Button colorPalette={"blue"} disabled={!uploadsComplete} asChild>
               {uploadsComplete ? (
-                <NextLink href={"/transcriptions"}>My transcriptions</NextLink>
+                <NextLink href={"/transcriptions"}>View transcriptions</NextLink>
               ) : (
-                <span>My transcriptions</span>
+                <span>View transcriptions</span>
               )}
             </Button>
           </Group>
