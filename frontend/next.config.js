1c5946931f4d288c3c26d00c695a0389fa8b3b7d
ScriptSmith <ScriptSmith@users.noreply.github.com>
2026-07-03 12:41:29 +1000

    Revert "Silence nextjs warning"
    This reverts commit 34689175a5bfd05f074cf5a72328dd7d5689530d.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require("node:path");
-
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  turbopack: {
-    root: path.join(__dirname, ".."),
-  },
 };
 
 module.exports = nextConfig;
