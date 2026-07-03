34689175a5bfd05f074cf5a72328dd7d5689530d
ScriptSmith <ScriptSmith@users.noreply.github.com>
2026-07-03 12:27:51 +1000

    Silence nextjs warning

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require("node:path");
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  turbopack: {
+    root: path.join(__dirname, ".."),
+  },
 };
 
 module.exports = nextConfig;
